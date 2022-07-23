accb7e59a0cd680c2c36b4bf1c973754cf0bad2d
kaisyQ <rutyo8@mail.ru>
2022-07-23 15:37:50 +1000

    header-component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react"
 import { BrowserRouter,Routes, Route } from "react-router-dom"
+
 
 import Header from "./components/Header/Header"
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react"
 
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+import { LinkContainer } from 'react-router-bootstrap'
+
 const Header = () => {
     return <>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <LinkContainer to='/'>
+                    <Navbar.Brand>Navbar</Navbar.Brand>
+                </LinkContainer>
+                <Nav className="me-auto">
+                    <LinkContainer to='/'>
+                        <Nav.Link>Home</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/news'>
+                        <Nav.Link>News</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/profile'>
+                        <Nav.Link>Profile</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+            </Container>
+        </Navbar>
     </>
 }
 
