ea0f70a96949c8c5a83408f0d7cb1fc0138ffde7
kaisyQ <rutyo8@mail.ru>
2022-07-24 10:11:36 +1000

    profileview/homeview--styles

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,9 +1,17 @@
 import React from "react"
+import styles from './HomeView.module.css'
 
 const HomeView = () => {
-    return <>
-        Home
-    </>
+    return <div className={`view-wrapper ${styles.homeContainer}`}>
+        <div className={styles.about}>
+            <h2>This is home page</h2>
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                Cumque sapiente numquam nihil nemo neque, cum error consequuntur.
+                Eveniet tenetur minus ab accusamus nisi aliquam sed quasi! Numquam ad dolorem rerum?
+            </p>
+        </div>
+    </div>
 }
 
 export default HomeView
--- src/views/ProfileView/ProfileView.jsx
+++ src/views/ProfileView/ProfileView.jsx
@@ -1,9 +1,13 @@
 import React from "react"
+import styles from './ProfileView.module.css'
 
 const ProfileView = (props) => {
-    return <>
-        Profile
-    </>
-}
+    return <div className="view-wrapper">
+        <div className="content">
+            <h4 className={styles.aboutHeader}>This is profile page</h4>
+            <h1 className={styles.welcome} >Welcome back {props.userData.username}</h1>
+        </div>   
+    </div>
+}   
 
 export default ProfileView
